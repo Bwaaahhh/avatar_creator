102562bdd43734ad14523c7d5e7bc88b8fc8e53d
S.Rebouh <rebouh.sarah@gmail.com>
2017-02-13 11:49:08 +0100

    add css

--- view/index.php
+++ view/index.php
@@ -1,1 +1,11 @@
-<p>lalala</p>
+<body>
+
+  <div id="creator" class="col-md-6">
+
+  </div>
+
+  <div id="last_creation" class="col-md-5">
+
+  </div>
+
+</body>
--- view/template/index.php
+++ view/template/index.php
@@ -2,15 +2,17 @@
 <html id="contenu">
     <head>
         <meta charset="utf-8">
-        <title>Blog-indiv</title>
-        <meta name="description" content="Travail Espace Membres" />
+        <title>Avatar creator de l'ACS Marmier</title>
+        <meta name="description" content="Avatar creator en groupe ACS" />
         <link rel="stylesheet" type="text/css" href="view/css/style.css"  />
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script type="text/javascript" src="view/js/js.js"></script>
     </head>
     <body>
         <header>
-            <h1>Blog de Cyril</h1>
+            <h1 id="title">Afoutar Creator de l'ACS Marmier</h1>
+            <h2 id="subtitle">Créez votre "avatar fou" avec les visages de l'ACS Marmier !</h2>
         </header>
         <main>
 
