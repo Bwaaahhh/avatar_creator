3f5e3e8fe3d01b50ee92cfab0c8209ee55029907
Unknown <sonya.grosjean0195@gmail.com>
2017-02-13 15:35:07 +0100

    Test twitter

--- view/index.php
+++ view/index.php
@@ -46,6 +46,10 @@
      <div class="col-md-6 envoyer">
        <input type="text" name="" value="URL de l'image" class="col-md-5 form"><button type="button" name="button" class="btn btn-default">Copier</button>
      </div>
+     
+     
+     <a href="https://twitter.com/share" class="twitter-share-button" data-size="large">Tweet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+     
 </div>
 
 
