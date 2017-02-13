06af2fcc3a28d2c5506275f7bb77189063eeca77
Bwaaahhh <cy.etievant@gmail.com>
2017-02-13 10:50:31 +0100

    upadate MVC

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+$page = "index.php";
+
+define('WEBROOT', str_replace($page, 'view/template/', $_SERVER['SCRIPT_NAME']));
+
+require_once("model/PDO.php");
+
+require_once('controller/class/Avatar.php');
+
+
+    $page = "view/index.php";
+
+
+    if (isset($_GET['page'])) {
+        $page = "view/".$_GET['page'].".php";
+    }
+
+
+
+include("view/template/index.php");
+
+
+
+// if(isset($_SESSION['user'])){
+
+
+
+    	// if (file_exists($page)) {
+    	// 	if ($page == "view/index.php") {
+    	// 		require_once("controller/get_all_articles.php");
+    	// 	}
+    	// 	if ($page == "view/article.php") {
+    	// 		require_once("controller/get_article.php");
+    	// 	}
+    		// include("view/template/index.php");
+    	// }
+    	// else {
+    	// 	echo("page non trouvée");
+    	// }
+// }
+// else{
+
+// }
---
+++ view/template/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html id="contenu">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog-indiv</title>
+        <meta name="description" content="Travail Espace Membres" />
+        <link rel="stylesheet" type="text/css" href="view/css/style.css"  />
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script type="text/javascript" src="view/js/js.js"></script>
+    </head>
+    <body>
+        <header>
+            <h1>Blog de Cyril</h1>
+        </header>
+        <main>
+
+
+<?php include($page);?>
+
+
+
+    <footer>
+
+    </footer>
+
+</body>
+</html>
