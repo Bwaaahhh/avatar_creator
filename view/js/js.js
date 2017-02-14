72ddb31071bde6f546c28201b3d617f8c72f86c4
Bwaaahhh <cy.etievant@gmail.com>
2017-02-14 11:41:23 +0100

    Update gen avatar

--- view/js/js.js
+++ view/js/js.js
@@ -1,13 +1,14 @@
-console.log("1");
+
 function getScreenshot(){
-    console.log("2");
     html2canvas($("#photoo"),{
         onrendered: function(canvas) {
+            $('.jssora12l').remove();
+            $('.jssora12r').remove();
+
             $('#last_creation').html("");
             $('#last_creation').append(canvas);
-
             $('#test').attr('href', canvas.toDataURL("images"));
-            $('#test').attr('download', 'Test file.png');
+            // $('#test').attr('download', 'Test file.png');
             $('#test')[0].click();
         }
     });
