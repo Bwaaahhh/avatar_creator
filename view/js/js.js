97be8de27c920b193ee6b71d43b33a89ac6cd642
S.Rebouh <rebouh.sarah@gmail.com>
2017-02-16 16:08:53 +0100

    js.js ok

--- view/js/js.js
+++ view/js/js.js
@@ -1,15 +1,33 @@
-
 function getScreenshot(){
-    html2canvas($("#photoo"),{
-        onrendered: function(canvas) {
-            $('.jssora12l').remove();
-            $('.jssora12r').remove();
 
-            $('#last_creation').html("");
-            $('#last_creation').append(canvas);
-            $('#test').attr('href', canvas.toDataURL("images"));
-            // $('#test').attr('download', 'Test file.png');
-            $('#test')[0].click();
+    var capture = {};
+
+    html2canvas($("#photoo"),{
+      onrendered: function(canvas) {
+        $("#test").attr('href' , canvas.toDataURL('image/png'));
+        $("#test").attr('download' , 'Votre avatar.png');
+        $('#test')[0].click();
+        capture.img = canvas.toDataURL( "image/png" );
+  			capture.data = { '#photoo' : capture.img };
+  			$.ajax({
+  			url: "controller/ajax.php",
+  			data: capture.data,
+  			type: 'post',
+  			success: function( result ) {
+          $("#url").val(result);
+  			}
+			});
         }
     });
+}
+
+
+//--------------------------- COPIER URL ---------------------------
+
+function copier()
+{
+  var url = document.querySelector('#url');
+  url.select();
+
+  document.execCommand('copy'); // cut
 }
